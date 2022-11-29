8ebbda2bcf3bdf274d8dafe795e2921afc695b56
Oleksandr Honcharov <o.v.goncharov90@gmail.com>
2022-11-29 15:41:13 +0200

    task done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,14 +9,27 @@ import ContactList from './ContactList/ContactList';
 
 export class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
+
+  componentDidMount() {
+    const checkLocalStorage = localStorage.getItem('contacts');
+
+    if (checkLocalStorage) {
+      const contacts = JSON.parse(checkLocalStorage);
+      this.setState({ contacts });
+    }
+  }
+
+  componentDidUpdate(prevState) {
+    const newContacts = this.state.contacts;
+    const prevContacts = prevState.contacts;
+
+    if (newContacts !== prevContacts) {
+      localStorage.setItem('contacts', JSON.stringify(newContacts));
+    }
+  }
 
   addContact = (name, number) => {
     if (
